48c43abf0f2b566135b3bef550811b62a0d330c0
ManilShangle <manilshangle@gmail.com>
2025-04-16 17:22:02 -0500

    Working Change 4/16

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
-import Main from './components/Main/Main.jsx';
+import Main from './pages/Main/Main.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import About from './pages/About/About.jsx';
 import Contact from './pages/Contact/Contact.jsx';
